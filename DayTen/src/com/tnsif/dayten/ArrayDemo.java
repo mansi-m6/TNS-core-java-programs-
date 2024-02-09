d49329ec3627391e8a6fa42be9fcae0baa738306
mansi-m06 <mansimahale22@gmail.com>
2024-02-09 13:37:10 +0530

    Arrays

---
+++ DayTen/src/com/tnsif/dayten/ArrayDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.dayten;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		float arr[]=new float[6];
+		int arr2[]=new int[6];
+		byte arr3[]=new byte[6];
+		double arr4[]=new double[6];
+		boolean arr5[]=new boolean[6];
+		
+		System.out.println(arr.getClass().getName());
+		System.out.println(arr2.getClass().getName());
+		System.out.println(arr3.getClass().getName());
+		System.out.println(arr4.getClass().getName());
+		System.out.println(arr5.getClass().getName());
+	}
+
+}
---
+++ DayTen/src/com/tnsif/dayten/MultiDimentionalArray.java
@@ -0,0 +1,17 @@
+package com.tnsif.dayten;
+
+public class MultiDimentionalArray {
+
+	public static void main(String[] args) {
+		int[][][] arr= {{{10,20,30},{40,50,60}},{{70,80},{90,100,110}}};
+		
+		System.out.println(arr[0][0][1]);
+		System.out.println(arr[0][1][2]);
+		System.out.println(arr[1][1][1]);
+		System.out.println(arr[1][0][1]);
+		
+		
+
+	}
+
+}
